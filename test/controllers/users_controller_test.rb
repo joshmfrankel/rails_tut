eacb9952db9b144d586e573b272615f71ffcda12
Josh Frankel <joshmfrankel@gmail.com>
2014-12-04 17:04:01 -0500

    Add resource routes for ser, Add custom test for show action of users controller, (7.1.4)

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,10 +1,23 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  test "should get new" do
+
+  def setup
+    @user = User.new(name: 'Mike Smith', email: 'test@test.com',
+                     password: 'foobar', password_confirmation: 'foobar')
+  end
+
+  test "should get NEW" do
     get :new
     assert_response :success
     assert_select 'title', full_title('Sign up')
+  end
+
+  test 'should get SHOW' do
+    @user.save
+    get :show, :id => @user.id
+    assert_response :success
+    assert_select 'title', full_title("#{@user.name}'s Profile")
   end
 
 end
