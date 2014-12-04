aa0d03e136208de1ef998d6dff385883685560e3
Josh Frankel <joshmfrankel@gmail.com>
2014-12-04 12:09:54 -0500

    User email indice, temporary removal of fixtures , addition of standard downcase for new emails

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141203211557) do
+ActiveRecord::Schema.define(version: 20141204165624) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -19,5 +19,7 @@ ActiveRecord::Schema.define(version: 20141203211557) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
