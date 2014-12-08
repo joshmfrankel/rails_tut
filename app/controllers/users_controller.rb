49d9e71fab961ed8737e9a95b004ad2a53d9c398
Josh Frankel <joshmfrankel@gmail.com>
2014-12-08 15:11:03 -0500

    User create strong parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
 
     if @user.save
       # sucess 
@@ -14,4 +14,11 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
