dd27bcc383df64edde408642f37beec0d902a898
Josh Frankel <joshmfrankel@gmail.com>
2015-01-10 12:31:45 -0500

    Up to 8.3

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -24,7 +24,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: 'Validpass' }
     end
 
-    
+
     assert_template "users/show"
+    assert is_logged_in?
   end
 end
